8b44604ecba57f259d5be2826aec53e0d84988ca
dev-yohannes <yohannesmulu.gm.dev@gmail.com>
2024-07-16 23:10:32 +0300

    status column added to users tabel

--- createTable.php
+++ createTable.php
@@ -7,22 +7,23 @@ $conn->select_db('registration');
 // $dropTable = "DROP TABLE IF EXISTS registrationTable";
 
 // if ($conn->query($dropTable) === TRUE) {
-//     echo "Table dropped successfully.";
+//     echo "Table dropped successfully. <br>";
 // } else {
-//     echo "Error dropping table: " . $conn->error;
+//     echo "Error dropping table: " . $conn->error . "<br>";
 // }
 
-$createTable = "CREATE TABLE IF NOT EXISTS (
-    tID INT(10) AUTO_INCREMENT PRIMARY KEY,
+$createTable = "CREATE TABLE IF NOT EXISTS registrationTable (
+    tID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     tUsername VARCHAR(30) NOT NULL,
     tFirstName VARCHAR(30) NOT NULL,
     tLastName VARCHAR(30) NOT NULL,
-    tEmail VARCHAR(30) NOT NULL,
-    tPassword VARCHAR (30) NOT NULL
+    tEmail VARCHAR(50) NOT NULL,
+    tPassword VARCHAR(255) NOT NULL,
+    tStatus INT(10) DEFAULT 0
 )";
 
-if ($conn->query($createTable) == TRUE) {
-    echo "Table created successfully. Or table already exists";
+if ($conn->query($createTable) === TRUE) {
+    echo "Table created successfully. Or table already exists.";
 } else {
-    echo "Error creating table " . $conn->error;
+    echo "Error creating table: " . $conn->error;
 }
