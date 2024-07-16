dfe4ef1d12c9c97a8ac9dfac16070121ab500f59
dev-yohannes <yohannesmulu.gm.dev@gmail.com>
2024-07-16 20:03:39 +0300

    tPassword column added

--- createTable.php
+++ createTable.php
@@ -4,12 +4,21 @@ include "connection.php";
 
 $conn->select_db('registration');
 
+// $dropTable = "DROP TABLE IF EXISTS registrationTable";
+
+// if ($conn->query($dropTable) === TRUE) {
+//     echo "Table dropped successfully.";
+// } else {
+//     echo "Error dropping table: " . $conn->error;
+// }
+
 $createTable = "CREATE TABLE IF NOT EXISTS registrationTable(
     tID INT(10) NOT NULL,
     tUsername VARCHAR(30) NOT NULL,
     tFirstName VARCHAR(30) NOT NULL,
     tLastName VARCHAR(30) NOT NULL,
-    tEmail VARCHAR(30) NOT NULL
+    tEmail VARCHAR(30) NOT NULL,
+    tPassword VARCHAR (30) NOT NULL
 )";
 
 if ($conn->query($createTable) == TRUE) {
